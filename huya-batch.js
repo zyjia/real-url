00e72a28f8077061ed83b64cb5103dcd571d0454
moxun33 <mxx0807@gmail.com>
2022-02-20 21:57:42 +0800

    fix 虎牙-动态房间列表

---
+++ huya-batch.js
@@ -0,0 +1,78 @@
+//批量
+var exec = require('child_process').execSync;
+const iconv = require('iconv-lite');//解码包，解决中文乱码问题
+const fs = require('fs')
+
+const fetch = require('node-fetch')
+
+const fireFetch = async (url, defaultRes = {}, isJson = false) => {
+
+
+    try {
+        const res = await fetch(url).then(res => isJson ? res.json() : res.text())
+        //  console.log(res);
+        return res
+    } catch (e) {
+        console.error(e)
+        return defaultRes
+    }
+}
+//指定子分区
+//获取第一页，可自行更改
+const getYqkRooms = async () => {
+    const tmpIds = [4201, 4183, 2067, 4061, 2079,]
+    const genUrl = (tmpId) => `https://live.cdn.huya.com/livelist/game/tagLivelist?gameId=2135&tmpId=${tmpId}&callback=getLiveListJsonpCallback&page=1`
+    const rooms = [];
+    for (const tmpId of tmpIds) {
+        console.log(`获取一起看 ${tmpId} 子分区  的房间列表`);
+        let resTxt = await fireFetch(genUrl(tmpId), { data: { datas: [] } }, false)
+        resTxt = resTxt.replace('getLiveListJsonpCallback(', '').replace('}})', '}}'), res = JSON.parse(resTxt)
+        const { data, status } = res;
+        if (status === 200) {
+            const list = data.datas || [];
+            const rs = list.map(({ profileRoom, introduction, nick, uid }) => ({ roomid: profileRoom, introduction, nick, uid }))
+            rooms.push(...rs)
+        }
+
+    }
+
+    return rooms;
+
+}
+
+(async () => {
+    const jsonList = [], rooms = [... await getYqkRooms()]
+
+    for (let i = 0; i < rooms.length; i++) {
+
+
+        const room = rooms[i], key = room.roomid
+        const stdout = exec(`python huya.py ${key}`)
+        const out = iconv.decode(stdout, 'cp936');
+        console.log(out);
+        if (out.includes('url') && out.includes('name')) {
+            const json = JSON.parse(out.replace(/\'/g, "\""))
+            json.name = room.nick ? `【${room.nick}】${room.introduction}` : json.name || '未知名称'
+            console.log('房间解析结果:', json);
+            jsonList.push(json)
+
+        } else {
+            console.log(key, '房间解析异常', out);
+        }
+
+    }
+
+    fs.writeFileSync(`./data/huya.json`, JSON.stringify(jsonList))
+    console.log('当前总数量', jsonList.length)
+
+
+    const m3u_list = ['#EXTM3U']
+    for (const i in jsonList) {
+        const obj = jsonList[i], url = obj['url1'] || obj['url2']
+        if (url) {
+            m3u_list.push(`#EXTINF:-1 group-title="虎牙", ${obj.name}`, url)
+        }
+    }
+
+    fs.writeFileSync(`./data/huya.m3u`, m3u_list.join('\n'))
+})()
--- huya.py
+++ huya.py
@@ -6,53 +6,69 @@ import base64
 import urllib.parse
 import hashlib
 import time
-
+import random
+import sys
 
 def live(e):
     i, b = e.split('?')
     r = i.split('/')
     s = re.sub(r'.(flv|m3u8)', '', r[-1])
-    c = b.split('&', 3)
+    c = b.split('&')
     c = [i for i in c if i != '']
     n = {i.split('=')[0]: i.split('=')[1] for i in c}
+    
     fm = urllib.parse.unquote(n['fm'])
     u = base64.b64decode(fm).decode('utf-8')
     p = u.split('_')[0]
     f = str(int(time.time() * 1e7))
     l = n['wsTime']
-    t = '0'
-    h = '_'.join([p, t, s, f, l])
+    mt = n['txyp']
+    t = str(random.randint(1460000000000, 1650000000000))
+    mm = t+f
+    ml = n['ctype']
+    fs = n['fs']
+    sp = n['sphdcdn']
+    spp = n['sphdDC']
+    spd = n['sphd']
+    ll = mm+'|'+ml+'|103'
+    ms = hashlib.md5(ll.encode("utf-8")).hexdigest()
+    h = '_'.join([p, t, s, ms, l])
     m = hashlib.md5(h.encode('utf-8')).hexdigest()
-    y = c[-1]
-    url = "{}?wsSecret={}&wsTime={}&u={}&seqid={}&{}".format(i, m, l, t, f, y)
+    urls = "{}?wsSecret={}&wsTime={}&seqid={}&ctype={}&ver=1&txyp={}&fs={}&&sphdcdn={}&sphdDC={}&sphd={}&t=103&ratio=0&u={}&t=103&sv=2110211124".format(i, m, l, mm, ml, mt, fs, sp, spp, spd, t)
+    aa, ab = urls.split('//')
+    url = 'https://'+ab
     return url
 
 
 def get_real_url(room_id):
+    room_url = 'https://mp.huya.com/cache.php?m=Live&do=profileRoom&roomid=' + str(room_id)
+    header = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) '
+                        'Chrome/75.0.3770.100 Mobile Safari/537.36 '
+    }
+    data = requests.get(url=room_url, headers=header).json()
+    multiLine=data['data']['stream']['flv']['multiLine']
+    #print(multiLine)
+    urls={}
+    liveData=data['data']['liveData']
+    
+    urls['name']=liveData['nick']+'-'+liveData['introduction']
+    for i in   range(len(multiLine)):
+        obj=multiLine[i]
+
+        liveline = live(obj['url'])
+        urls['url'+str(i+1)]=liveline
+    return urls
+
+
+if __name__ == '__main__':
     try:
-        room_url = 'https://m.huya.com/' + str(room_id)
-        header = {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'User-Agent': 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/75.0.3770.100 Mobile Safari/537.36 '
-        }
-        response = requests.get(url=room_url, headers=header).text
-        liveLineUrl = re.findall(r'"liveLineUrl":"([\s\S]*?)",', response)[0]
-        liveline = base64.b64decode(liveLineUrl).decode('utf-8')
-        if liveline:
-            if 'replay' in liveline:
-                return '直播录像：' + liveline
-            else:
-                liveline = live(liveline)
-                real_url = ("https:" + liveline).replace("hls", "flv").replace("m3u8", "flv")
-        else:
-            real_url = '未开播或直播间不存在'
-    except:
-        real_url = '未开播或直播间不存在'
-    return real_url
-
-
-rid = input('输入虎牙直播房间号：\n')
-real_url = get_real_url(rid)
-print('该直播间源地址为：')
-print(real_url)
+        try:
+            r=sys.argv[1]
+        except:
+            r = input('请输入虎牙直播房间号：\n')
+        print(get_real_url(r))
+    except Exception as e:
+        print(e)    
+        
